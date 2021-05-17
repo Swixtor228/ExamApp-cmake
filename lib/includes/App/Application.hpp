17ae96e2ae9ef8c977ce156802556c22a517768a
Not Secret <43242733+Swixtor228@users.noreply.github.com>
2021-05-17 19:18:39 +0300

    File Application

---
+++ lib/includes/App/Application.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+#include <App/MassiveEditor.hpp>
+#include <App/LibraryApp.hpp>
+
+namespace Application {
+	using namespace MassiveEditor; 
+	using namespace LibraryApp;
+
+	void StartScreen();
+}
