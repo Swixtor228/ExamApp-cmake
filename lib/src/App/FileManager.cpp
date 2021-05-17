af7ab2c7e878f091836e5c1fbdfcbb2a87367fa1
Not Secret <43242733+Swixtor228@users.noreply.github.com>
2021-05-17 19:24:47 +0300

    .cpp file

---
+++ lib/src/App/FileManager.cpp
@@ -0,0 +1,66 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+#include <App/FileManager.hpp>
+#include <App/LibraryApp.hpp>
+#include <App/Log.hpp>
+
+void FileManager::GetFileData(bool isFilled, LibraryApp::Book book[], int bookSize)
+{
+	if (isFilled)
+	{
+		system("cls");
+		string name;
+
+		cout << "File name: ";
+		cin >> name;
+
+		FileManager::SaveToFile(name, book, bookSize);
+	}
+	else
+		LibraryApp::checkIsFilled(isFilled, LibraryApp::LibraryAppStartScreen);
+}
+
+void FileManager::SaveToFile(std::string name, LibraryApp::Book book[], int bookSize)
+{
+	ofstream output("../../logs/input/" + name + ".txt", ios::out);
+	if (output.is_open())
+	{
+		output << "[==============================]\n";
+		for (int i = 0; i < bookSize; i++)
+		{
+			output << "Book #" << i << endl;
+			output << "* Title: " << book[i].title << endl;
+			output << "* Author name: " << book[i].authorName << endl;
+			output << "* Publisher: " << book[i].publisher << endl;
+			output << "* Genre: " << book[i].genre << endl;
+			output << "[==============================]\n";
+		}
+		output.close();
+		LOG_INFO("File {}.txt was saved successfully!\n", name);
+		system("pause");
+		LibraryApp::LibraryAppStartScreen();
+	}
+}
+
+void FileManager::readFromFile(string name, vector<int> arr)
+{
+	int intLine = 0;
+	string line;
+	ifstream input("../../logs/output/test.txt" /*+ name + ".txt"*/);
+
+	if (input.is_open())
+	{
+		while (getline(cin, line))
+		{
+			cout << line << " ";
+		}
+	}
+
+	input.close();
+	LOG_INFO("Readed successfully!");
+}
